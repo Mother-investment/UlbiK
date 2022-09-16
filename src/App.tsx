dd81443391da47f97c04110f602a33d2fe232ea6
Mother-investment <yurii.antipov01@gmail.com>
2022-09-16 14:47:10 +0700

    themes[7]

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import { Suspense } from 'react'
+import { Suspense, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { Route, Routes } from 'react-router-dom'
-import './index.scss'
+import './styles/index.scss'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
 import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import { ThemeContext } from './theme/ThemeContext'
+import { useTheme } from './theme/useTheme'
 
-type AppProps = {}
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark'
+}
 
-const App: React.FC<AppProps> = () => {
+const App: React.FC = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button className='toggleTheme' onClick={toggleTheme}>Toggle Theme</button>
+            <br />
             <Link to='/'>Main</Link>
             <Link to='/about'>About</Link>
             <Suspense fallback={<div>Loading..</div>}>
