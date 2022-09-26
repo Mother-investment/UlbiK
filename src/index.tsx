11a4a2953311cd77f8747174589eaf59440659c4
Mother-investment <yurii.antipov01@gmail.com>
2022-09-26 21:22:34 +0700

    ErrorBoundary[22]

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './app/App'
 
 import 'shared/config/i18n/i18n'
+import { ErrorBoundary } from 'app/providers/ErrorBoundary'
 
 render(
 	<BrowserRouter>
-		<ThemeProvider>
-			<App />
-		</ThemeProvider>
+		<ErrorBoundary>
+			<ThemeProvider>
+				<App />
+			</ThemeProvider>
+		</ErrorBoundary>
 	</BrowserRouter>,
 	document.getElementById('root')
 )
