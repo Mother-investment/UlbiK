af2e619d83386dee5ead467ab718980c8825f07e
Mother-investment <yurii.antipov01@gmail.com>
2022-09-26 22:05:13 +0700

    BundleAnalyzer[23]

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import webpack from 'webpack'
 import HTMLWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 import type { WebpackPluginInstance } from 'webpack'
 import type { BuildOptions } from './types/config'
@@ -12,6 +13,7 @@ export function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInsta
 		new webpack.ProgressPlugin(),
 		new MiniCssExtractPlugin({ filename:'css/[name].[contenthash:8].css', chunkFilename:'css/[name].[contenthash:8].css' }),
 		new webpack.DefinePlugin({ __IS_DEV__: JSON.stringify(isDev) }),
-		isDev && new ReactRefreshWebpackPlugin()
+		isDev && new ReactRefreshWebpackPlugin(),
+		new BundleAnalyzerPlugin({openAnalyzer: false})
 	]
 }
