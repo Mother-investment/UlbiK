0a13f31bc4ebe3fa480fe2460c4eb85b641dd739
Mother-investment <yurii.antipov01@gmail.com>
2023-01-07 22:40:37 +0700

    fix langSwitcher

--- src/features/EditProfile/ui/EditForm/EditForm.tsx
+++ src/features/EditProfile/ui/EditForm/EditForm.tsx
@@ -3,6 +3,7 @@ import cls from './EditForm.module.scss'
 import { memo } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Text } from 'shared/ui/Text/Text'
+import { Input } from 'shared/ui/Input/Input'
 
 interface EditFormProps {
 	className?: string
@@ -13,9 +14,34 @@ export const EditForm:React.FC<EditFormProps> = memo((props) => {
 	const { t } = useTranslation()
 
 	return (
-		<div className={classNames(cls.EditForm, {}, [className])}>
+		<form className={classNames(cls.EditForm, {}, [className])}>
 			<Text title={t('Редактировать профиль')} />
-			
-		</div>
+			<div className={cls.item}>
+				<Text text={t('Имя')} />
+				<Input />
+			</div>
+			<div className={cls.item}>
+				<Text text={t('Фамилия')} />
+				<Input />
+			</div>
+			<div className={cls.item}>
+				<Text text={t('День рождения')} />
+				<Input type='date'/>
+			</div>
+			<div className={cls.item}>
+				<Text text={t('Страна')} />
+				<select>
+					<option>{t('Россия')}</option>
+					<option>{t('США')}</option>
+				</select>
+			</div>
+			<div className={cls.item}>
+				<Text text={t('Город')} />
+				<select>
+					<option>{t('Новосибирск')}</option>
+					<option>{t('Москва')}</option>
+				</select>
+			</div>
+		</form>
 	)
 })
